a29c044ddf842fcd0a4390c779a5872cf6687098
Allen Phelps <allenatmc@gmail.com>
2015-09-02 20:32:54 -0500

    switching to Sass, Bootstrap, Requirejs

---
+++ javascripts/entry.js
@@ -0,0 +1,15 @@
+requirejs.config({
+	baseUrl: "./javascripts",
+	paths:{
+"jquery": "../bower_components/jquery/dist/jquery.min",
+"bootstrap": "/bower_components/bootstrap/dist/js/bootstrap.min"
+	},
+
+"shim": {
+        "bootstrap": ["jquery"]
+  }
+
+});
+
+require(['sandwichMaker'], function() {} );
+
